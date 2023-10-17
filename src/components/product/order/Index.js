7691a5045d3cb499cb6ad7c1d6d13a1adcd51e0f
mengbi <mengbi1314@outlook.com>
2023-10-17 12:00:49 +0800

    我的订单二

--- src/components/product/order/Index.js
+++ src/components/product/order/Index.js
@@ -14,12 +14,19 @@ const Index = () => {
     const limit = React.useRef(10);
     const [status, setStatus] = React.useState(0);
 
-
-
-
     // 拉下刷新
     const onRefresh = () => {
-
+        setList([]);
+        LeaseData.current = [];
+        setFinished(false);
+        page.current = 1;
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                React.Vant.Toast.info('刷新成功');
+                getLeaseData();
+                resolve(true);
+            }, 1500);
+        });
     }
 
     // 上拉加载
@@ -32,20 +39,97 @@ const Index = () => {
     const getLeaseData = async () => {
         let data = {
             busid: LoginBusiness.id,
-
+            page: page.current,
+            limit: limit.current,
+            status: status
         }
 
         let result = await React.Api.OrderIndex(data);
+
+        if (result) {
+            if (result.code === 0) {
+                setFinished(true);
+                React.Vant.Toast.fail(result.msg);
+                return;
+            }
+
+            let count = result.data.count;
+            LeaseData.current = LeaseData.current.concat(result.data.list);
+            setList(LeaseData.current);
+
+            if (LeaseData.current.length >= count) {
+                setFinished(true);
+                return;
+            }
+
+            page.current = page.current + 1;
+            setFinished(false);
+        }
+
     }
 
+    const ConfirmLease = () => {
+
+    }
 
     const Items = () => {
+        if (list.length > 0) {
+            return list.map((item, key) => {
+                return (
+                    <li key={key}>
+                        <div className="vip_order_goods">
+                            <h3>
+                                <i><img src={item.product.thumb_cdn} /></i>
+                                <dl>
+                                    <dt>{item.product.name}</dt>
+                                    <dd>
+                                        <em>押金</em>
+                                        <em>{item.rent}</em>
+                                    </dd>
+                                    <dd>
+                                        <em>总价</em>
+                                        <em>{item.price}</em>
+                                    </dd>
+                                    <dd>
+                                        <em>结束时间</em>
+                                        <em>{item.endtime_text}</em>
+                                    </dd>
+                                    <dd>
+                                        <em>订单状态</em>
+                                        <em>{item.status_text}</em>
+                                    </dd>
+                                </dl>
+                            </h3>
+                        </div>
 
+                        <div className="order_btn">
+
+                            {(item.status == 5) && <React.Vant.Button round type="warning" size="small" onClick={() => Navigate(`/order/order/comment?id=${item.id}`)}>订单评价</React.Vant.Button>}
+
+                            {(item.status == 3 || item.status == 4) && <React.Vant.Button round type="danger" size="small" onClick={() => Navigate(`/order/order/recovery?id=${item.id}`)}>归还商品</React.Vant.Button>}
+
+                            {item.status == 2 && <React.Vant.Button round type="warning" size="small" onClick={() => ConfirmLease(item.id)}>确认收货</React.Vant.Button>}
+
+                            {item.status >= 2 && <React.Vant.Button round type="info" size="small" onClick={() => Navigate(`/order/order/express?id=${item.id}`)}>查看物流</React.Vant.Button>}
+
+                            <React.Vant.Button round type="primary" size="small" onClick={() => Navigate(`/order/order/info?id=${item.id}`)}>查看详情</React.Vant.Button>
+                        </div>
+                    </li>
+                )
+            })
+        }
     }
 
-    const onTabsChange = () => {
-
+    const onTabsChange = (value) => {
+        setStatus(value.name);
     }
+
+    React.useEffect(() => {
+        setList([]);
+        LeaseData.current = [];
+        setFinished(false);
+        page.current = 1;
+    }, [status]);
 
     // 定义选项卡的数据
     const statusList = [
